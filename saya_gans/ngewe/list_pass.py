05cbf0375e4899407b31b73f195ac230d0ab46bf
no name <84274139+itsuki10@users.noreply.github.com>
2021-07-10 18:27:10 +0700

    Update list_pass.py

--- saya_gans/ngewe/list_pass.py
+++ saya_gans/ngewe/list_pass.py
@@ -1,14 +1,12 @@
 # hallo bro :v
 
-pw_tambahan="""
-
-kontol|sayang
-
-""".strip().split("|")
+pw_tambahan="kontol|sayang".split("|")
 ngntd=((())>(()))+((())>(()))
+
 def awok(ngentod):
 	return [ngentod,ngentod+"123",ngentod+"12345"]
-def pw_list(ngentot):
+
+def pw_list(ngentot,method=None):
 	if len(ngentot)==2:
 		aap_afandi=ngentot[1].split(" ")
 		if len(aap_afandi[ngntd])!=ngntd:
@@ -20,4 +18,6 @@ def pw_list(ngentot):
 				[asu.append(x) for x in pw_tambahan]
 		else: asu=pw_tambahan
 	else: asu=pw_tambahan
+	if "api" in str(method):
+		[asu.append(x) for x in ["anjing","bangsat","bismillah","rahasia","Indonesia"]]
 	return asu
