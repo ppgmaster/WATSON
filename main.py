2f21bfa0438d337c3272b3e5cde2896f3655634d
ppgmaster <92914392+ppgmaster@users.noreply.github.com>
2021-10-22 14:14:52 +0700

    watson main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ runtah=["surya_gans/__pycache__","modul/__pycache__","wibu/__pycache__","saya_ga
 if sys.version[0]!="3":
 	exit(" ! harap gunakan python3")
 
-from saya_gans import awokawokawok
+from surya_gans import awokawokawok
 try: [shutil.rmtree(x) for x in runtah]
 except: pass
 awokawokawok()
